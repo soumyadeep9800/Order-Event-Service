053d1f397cb32479e69434837659a3680fa39b5d
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-09-05 21:16:16 +0530

    add orderService

---
+++ src/main/java/com/ecommerce/orderevent/service/OrderService.java
@@ -0,0 +1,62 @@
+package com.ecommerce.orderevent.service;
+
+import com.ecommerce.orderevent.entity.MenuItem;
+import com.ecommerce.orderevent.entity.Order;
+import com.ecommerce.orderevent.entity.Restaurant;
+import com.ecommerce.orderevent.entity.User;
+import com.ecommerce.orderevent.repository.MenuItemRepository;
+import com.ecommerce.orderevent.repository.OrderRepository;
+import com.ecommerce.orderevent.repository.RestaurantRepository;
+import com.ecommerce.orderevent.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+    private final RestaurantRepository restaurantRepository;
+    private final MenuItemRepository menuItemRepository;
+
+    public OrderService(OrderRepository orderRepository,
+                        UserRepository userRepository,
+                        RestaurantRepository restaurantRepository,
+                        MenuItemRepository menuItemRepository){
+        this.orderRepository=orderRepository;
+        this.restaurantRepository=restaurantRepository;
+        this.userRepository=userRepository;
+        this.menuItemRepository=menuItemRepository;
+    }
+
+    public Order placeOrder(Long userId, Long restaurantId, List<Long> menuItemIds){
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new RuntimeException("User Not Found!"));
+        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(()-> new RuntimeException("Restaurant Not Found!"));
+
+        List<MenuItem> items = menuItemRepository.findAllById(menuItemIds);
+        if(items.isEmpty()) throw new RuntimeException("No valid menu items found!");
+        //calculate total price
+        double totalPrice = items.stream().mapToDouble(MenuItem::getPrice).sum();
+
+        Order order = new Order();
+        order.setUser(user);
+        order.setRestaurant(restaurant);
+        order.setOrderDate(LocalDateTime.now());
+        order.setStatus("PLACED");
+        order.setTotalPrice(totalPrice);
+
+        return orderRepository.save(order);
+    }
+
+    public List<Order> getUserOrders(Long userId){
+        return orderRepository.findByUserId(userId);
+    }
+
+    public void cancelOrder(Long orderId){
+        orderRepository.deleteById(orderId);
+    }
+}
