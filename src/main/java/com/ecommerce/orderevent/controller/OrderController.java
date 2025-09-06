ce18b60f156a407f758b7597a2edd89acc81f872
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-09-06 18:44:33 +0530

    add orderController

--- src/main/java/com/ecommerce/orderevent/controller/OrderController.java
+++ src/main/java/com/ecommerce/orderevent/controller/OrderController.java
@@ -2,20 +2,41 @@ package com.ecommerce.orderevent.controller;
 
 import com.ecommerce.orderevent.entity.Order;
 import com.ecommerce.orderevent.service.OrderService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
     public  OrderController(OrderService orderService){
         this.orderService=orderService;
     }
 
-//    @PostMapping("/place-order")
-//    public Order placeOrder(){
-//
-//    }
+    @PostMapping()
+    public ResponseEntity<Order> placeOrder(@RequestBody Map<String, Object> request){
+        Long userId = Long.valueOf(request.get("UserId").toString());
+        Long restaurantId = Long.valueOf(request.get("restaurantId").toString());
+
+        @SuppressWarnings("unchecked")
+        List<Long> menuItemIds = (List<Long>) request.get("menuitemIds");
+
+        Order order = orderService.placeOrder(userId, restaurantId, menuItemIds);
+        return ResponseEntity.ok(order);
+    }
+
+    @GetMapping("user/{userId}")
+    public ResponseEntity<List<Order>> getUserOrders(@PathVariable Long userId){
+        List<Order> orders = orderService.getUserOrders(userId);
+        return ResponseEntity.ok(orders);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Void> cancelOrder(@PathVariable Long orderId){
+        orderService.cancelOrder(orderId);
+        return ResponseEntity.noContent().build();
+    }
 }
