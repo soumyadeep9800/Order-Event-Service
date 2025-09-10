8dadb69a825c8a60a3bc83948ac92387e6d38564
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-09-10 21:01:53 +0530

    refactord the publicController

--- src/main/java/com/ecommerce/orderevent/controller/PublicController.java
+++ src/main/java/com/ecommerce/orderevent/controller/PublicController.java
@@ -26,32 +26,29 @@ public class PublicController {
 
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<User>> registerUser(@RequestBody User user) {
-        try {
-            User savedUser = userService.saveUser(user);
-            ApiResponse<User> response = new ApiResponse<>("success", "User saved successfully!", savedUser, LocalDateTime.now());
+            User savedUser = userService.saveUser(user); // throws ResourceNotFoundException if not found
+            ApiResponse<User> response = new ApiResponse<>(
+                    "success",
+                    "User saved successfully!",
+                    savedUser,
+                    LocalDateTime.now()
+            );
             return ResponseEntity.status(HttpStatus.CREATED).body(response); // 201 Created
-        } catch (IllegalArgumentException ex) {
-            ApiResponse<User> response = new ApiResponse<>("error", ex.getMessage(),  null, LocalDateTime.now());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(response); // 409 Conflict
-        }
     }
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<User>> login(@RequestBody Map<String,String> loginRequest){
         String email = loginRequest.get("email");
         String password = loginRequest.get("password");
-        try {
-            User user = userService.getByEmail(email);
-            ApiResponse<User> response = new ApiResponse<>("success", "User login successfully!", user, LocalDateTime.now());
-            if (user.getPassword().equals(password)) {
-                return ResponseEntity.ok(response);     // 200
-            } else {
-                response = new ApiResponse<>("failed", "User login failed!", user, LocalDateTime.now());
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);  // 401
-            }
-        } catch (IllegalArgumentException ex) {
-            ApiResponse<User> response = new ApiResponse<>("failed", ex.getMessage(), null, LocalDateTime.now());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response); // 404
-        }
+
+        User user = userService.getByEmail(email); // throws ResourceNotFoundException if not found
+        if (!user.getPassword().equals(password)) throw new IllegalArgumentException("Invalid password!");
+
+        ApiResponse<User> response = new ApiResponse<>(
+                "success",
+                "User login successfully!",
+                user, LocalDateTime.now()
+        );
+        return ResponseEntity.ok(response);     // 200
     }
 }
