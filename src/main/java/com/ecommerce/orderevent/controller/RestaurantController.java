05a07ca1a1d4321fe685838edf6a1711fd0ea2ab
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-09-11 18:25:22 +0530

    Refactor the Restaurant controller

--- src/main/java/com/ecommerce/orderevent/controller/RestaurantController.java
+++ src/main/java/com/ecommerce/orderevent/controller/RestaurantController.java
@@ -1,12 +1,16 @@
 package com.ecommerce.orderevent.controller;
 
+import com.ecommerce.orderevent.dto.ApiResponse;
+import static com.ecommerce.orderevent.constants.ApiResponseStatus.SUCCESS;
 import com.ecommerce.orderevent.entity.MenuItem;
 import com.ecommerce.orderevent.entity.Restaurant;
 import com.ecommerce.orderevent.service.MenuItemService;
 import com.ecommerce.orderevent.service.RestaurantService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
 import java.util.*;
 
 @RestController
@@ -21,41 +25,76 @@ public class RestaurantController {
         this.menuItemService=menuItemService;
     }
 
-    @PostMapping
-    public Restaurant addNewRestaurant(@RequestBody Restaurant restaurant){
-        return restaurantService.addRestaurant(restaurant);
+    @PostMapping     //@Operation(summary = "Fetch restaurant by ID", description = "Get details of a specific restaurant")
+    public ResponseEntity<ApiResponse<Restaurant>> addNewRestaurant(@RequestBody Restaurant restaurant){
+        Restaurant saveRestaurant = restaurantService.addRestaurant(restaurant);
+        ApiResponse<Restaurant> response = new ApiResponse<>(
+                SUCCESS,
+                "Restaurant added successfully!",
+                saveRestaurant,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public List<Restaurant> getAllRestaurant(){
-        return restaurantService.getAllRestaurant();
+    public ResponseEntity<ApiResponse<List<Restaurant>>> getAllRestaurant(){
+        List<Restaurant> allRestaurant = restaurantService.getAllRestaurant();
+        ApiResponse<List<Restaurant>> response = new ApiResponse<>(
+                SUCCESS,
+                "All restaurants fetched successfully!",
+                allRestaurant,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Restaurant> getRestaurantById(@PathVariable Long id){
+    public ResponseEntity<ApiResponse<Restaurant>> getRestaurantById(@PathVariable Long id){
         Restaurant restaurant = restaurantService.getRestaurantById(id);
-        return ResponseEntity.ok(restaurant);
+        ApiResponse<Restaurant> response = new ApiResponse<>(
+                SUCCESS,
+                "Restaurant details fetched successfully!",
+                restaurant,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{id}/menu")
-    public List<MenuItem> menuOfRestaurant(@PathVariable Long id){
-        return menuItemService.getMenuItemsByRestaurant(id);
+    public ResponseEntity<ApiResponse<List<MenuItem>>> menuOfRestaurant(@PathVariable Long id){
+        List<MenuItem> listOfMenus = menuItemService.getMenuItemsByRestaurant(id);
+        ApiResponse<List<MenuItem>> response = new ApiResponse<>(
+                SUCCESS,
+                "Menu-items of the restaurant fetched successfully!",
+                listOfMenus,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Restaurant> updateRestaurant(@PathVariable Long id, @RequestBody Restaurant updateRestaurant){
+    public ResponseEntity<ApiResponse<Restaurant>> updateRestaurant(@PathVariable Long id, @RequestBody Restaurant updateRestaurant){
         Restaurant restaurant = restaurantService.updateRestaurant(id, updateRestaurant);
-        return ResponseEntity.ok(restaurant);
+        ApiResponse<Restaurant> response = new ApiResponse<>(
+                SUCCESS,
+        "Update restaurant successfully!",
+                restaurant,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteRestaurant(@PathVariable Long id){
-        try{
-            restaurantService.deleteRestaurant(id);
-            return "Restaurant Delete Successfully!";
-        } catch (Exception e) {
-            return " Restaurant can not deleted "+ e;
-        }
+    public ResponseEntity<ApiResponse<Void>> deleteRestaurant(@PathVariable Long id) {
+        restaurantService.deleteRestaurant(id);
+        ApiResponse<Void> response = new ApiResponse<>(
+                SUCCESS,
+                "Restaurant deleted successfully!",
+                null,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
     }
 
 }
