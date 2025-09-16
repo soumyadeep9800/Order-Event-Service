d830fd602229a5f7ec7f994ae2c46d289bfbc2c8
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-09-16 23:15:39 +0530

    add Operation

--- src/main/java/com/ecommerce/orderevent/controller/MenuItemController.java
+++ src/main/java/com/ecommerce/orderevent/controller/MenuItemController.java
@@ -6,6 +6,7 @@ import com.ecommerce.orderevent.dtos.MenuItemRequestDto;
 import com.ecommerce.orderevent.dtos.MenuItemResponseDto;
 import com.ecommerce.orderevent.entity.MenuItem;
 import com.ecommerce.orderevent.service.MenuItemService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,7 @@ public class MenuItemController {
     }
 
     @PostMapping
+    @Operation(summary = "Add new menu item", description = "Create and save a new menu item for a restaurant")
     public ResponseEntity<ApiResponse<MenuItemResponseDto>> addMenuItem(@RequestBody MenuItemRequestDto menuItemRequestDto){
         Long id = menuItemRequestDto.getRestaurantId();
         MenuItemResponseDto getMenuitem = menuItemService.addMenuItem(id,menuItemRequestDto);
@@ -36,6 +38,7 @@ public class MenuItemController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update menu item", description = "Update the details of an existing menu item by its ID")
     public ResponseEntity<ApiResponse<MenuItemResponseDto>> updateMenuItem(@PathVariable Long id, @RequestBody MenuItemRequestDto menuItemRequestDto){
         MenuItemResponseDto updatedMenu = menuItemService.updateMenuItem(id, menuItemRequestDto);
         ApiResponse<MenuItemResponseDto> response = new ApiResponse<>(
@@ -48,6 +51,7 @@ public class MenuItemController {
     }
 
     @GetMapping("/menu-items/{id}")
+    @Operation(summary = "Fetch menu item by ID", description = "Retrieve details of a specific menu item by its ID")
     public ResponseEntity<ApiResponse<MenuItem>> getMenuItem(@PathVariable Long id) {
         MenuItem menuItem = menuItemService.findMenuItem(id);
         ApiResponse<MenuItem> response = new ApiResponse<>(
@@ -60,6 +64,7 @@ public class MenuItemController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete menu item", description = "Remove an existing menu item from the system by its ID")
     public ResponseEntity<ApiResponse<Void>> deleteMenuItem(@PathVariable Long id){
         menuItemService.deleteMenuItem(id);
         ApiResponse<Void> response = new ApiResponse<>(
