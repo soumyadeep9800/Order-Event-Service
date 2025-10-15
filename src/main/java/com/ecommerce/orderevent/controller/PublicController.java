3c376ed233ddcfdf17e900db0f3face43ba7ec3c
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-10-15 10:22:20 +0530

    add generete token

--- src/main/java/com/ecommerce/orderevent/controller/PublicController.java
+++ src/main/java/com/ecommerce/orderevent/controller/PublicController.java
@@ -108,32 +108,3 @@ public class PublicController {
         }
     }
 }
-
-//    @PostMapping("/register")
-//    @Operation(summary = "Register new user", description = "Create a new user account and save it to the system")
-//    public ResponseEntity<ApiResponse<UserResponseDto>> registerUser(@RequestBody UserRequestDto requestDto) {
-//        UserResponseDto savedUser = userService.saveUser(requestDto); // throws ResourceNotFoundException if not found
-//            ApiResponse<UserResponseDto> response = new ApiResponse<>(
-//                    SUCCESS,
-//                    "User saved successfully!",
-//                    savedUser,
-//                    LocalDateTime.now()
-//            );
-//            return ResponseEntity.status(HttpStatus.CREATED).body(response); // 201 Created
-//    }
-
-//    @PostMapping("/login")
-//    @Operation(summary = "User login", description = "Authenticate a user with email and password")
-//    public ResponseEntity<ApiResponse<UserResponseDto>> login(@RequestBody UserRequestDto requestDto) {
-//        User user = userService.getByEmail(requestDto.getEmail());
-//        if (!user.getPassword().equals(requestDto.getPassword())) throw new IllegalArgumentException("Invalid password!");
-//
-//        UserResponseDto responseDto = UserResponseDto.fromEntity(user);
-//        ApiResponse<UserResponseDto> response = new ApiResponse<>(
-//                SUCCESS,
-//                "User login successfully!",
-//                responseDto,
-//                LocalDateTime.now()
-//        );
-//        return ResponseEntity.ok(response); //200
-//    }
